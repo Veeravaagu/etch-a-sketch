04dd3fb5e6476092751977c62cd034125b2385c5
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-09 11:35:21 +0530

    Create EventListener for all buttons and use conditionals to choose color
    Pick Random Color with Math.Random for rainbow button
    Use style 'null' as backgroundColor for clear button
    Add if loop to get numbers between 0-100.

--- main.js
+++ main.js
@@ -1,43 +1,93 @@
+let colorPicker;
+
 let divBoard = document.querySelector('#board-container');
+
+let eraserButton = document.querySelector('.eraserButton')
+
+let blackButton = document.querySelector('.blackButton');
+
+let rainbowButton = document.querySelector('.rainbowButton');
+
+let clearButton = document.querySelector('.clearButton');
 
 let sizeofBoard = prompt("Enter the number of squares that you want");
 
+eraserButton.addEventListener('click', ()=>{colorPicker = 1;}); 
+
+blackButton.addEventListener('click', ()=>{colorPicker = 2;});
+
+rainbowButton.addEventListener('click',()=>{colorPicker = 3;});
+
+divBoard.addEventListener('mousemove', function (event){
+    clearButton.addEventListener('click',()=>{
+        event.target.style.backgroundColor = null;})})
+// clearButton.addEventListener('click',()=>{colorPicker = 4;
+// });
+
 function size (number,measurements) {
-    
+
     number =  sizeofBoard;
-    
-    measurements = (500/number);
-    
-    console.log(measurements);
 
-
-    for(let i = 0 ; i < number*number ; i++)
-
+    if (number <= 100 && number > 0)
     {
     
-        let square = document.createElement ('div');
+        measurements = (500/number);
     
-        square.setAttribute('id',`${i + 1}divSquare`);
-    
-        square.style.border = '0.5px solid black';
-    
-        square.style.width = `${measurements}px`;
-    
-        square.style.height = `${measurements}px`;
-    
-        divBoard.appendChild(square);
-    
-        console.log(square);
+        for(let i = 0 ; i < number*number ; i++)
 
-        divBoard.addEventListener('mousemove', function (e)
-                                                  {if (e.target.id === `${i + 1}divSquare`) {square.style.backgroundColor='white';
-                                                                                            console.log(`clicked ${i + 1}divSquare`);}})
+        {
+            let square = document.createElement ('div');
+
+            square.setAttribute('id',`${i + 1}divSquare`);
     
+            square.style.border = '0.5px solid black';
+            
+            square.style.width = `${measurements}px`;
+            
+            square.style.height = `${measurements}px`;
+
+            divBoard.appendChild(square);
+
+            const randRed = Math.floor(Math.random() * 256);
+    
+            const randGreen = Math.floor(Math.random() * 256);
+    
+            const randBlue = Math.floor(Math.random() * 256);
+
+            divBoard.addEventListener('mousemove', function (e)
+                                                
+            {
+            
+                if (e.target.id === `${i + 1}divSquare` && colorPicker === 1) 
+            
+                {square.style.backgroundColor = 'white';}
+                
+                else if(e.target.id === `${i + 1}divSquare` && colorPicker === 2)
+
+                {square.style.backgroundColor = 'black';}
+
+                else if(e.target.id === `${i + 1}divSquare` && colorPicker === 3)
+
+                {e.target.style.backgroundColor = `rgb(${randRed}, ${randGreen}, ${randBlue})`;}
+                
+
+            }
+            )
+
+        }
+                                        
     }
-
-}
-
+    else
+    
+    {
+        prompt("Enter a number less than 100 and above 0, no invalid numbers allowed!")
+    }
+                                            
+    }
 size();
+
+
+
 
 
 
